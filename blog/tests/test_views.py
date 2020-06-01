e66d3dbf588c377282364d46bf23497546d22b9e
90houlaoheshang <907333918@qq.com>
2020-06-01 11:15:51 +0800

    测试登录功能的view

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
@@ -9,3 +10,16 @@ class IndexViewTest(TestCase):
         response = self.client.get('')
         self.assertEqual(response.status_code, 301)
         self.assertEqual('/' + response.url, reverse('index'))
+
+
+class LoginViewTest(TestCase):
+    def setUp(self) -> None:
+        testuser = User.objects.create_user(username='testuser', password='bdakfkweqhfewfbds')
+        testuser.save()
+
+    def test_index_logged_in(self):
+        login_res = self.client.login(username='testuser', password='bdakfkweqhfewfbds')
+        self.assertEqual(login_res, True)
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(response.context['user']), 'testuser')
