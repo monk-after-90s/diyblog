4c2c6291524d9d9aa524eba9704d7475ec479eb1
90houlaoheshang <907333918@qq.com>
2020-06-02 10:52:50 +0800

    添加博客和评论model

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,23 @@ from django.db import models
 from django.urls import reverse
 
 
+# Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=50, help_text='Enter the name of the blog.')
+    author = models.ForeignKey('BlogUser', on_delete=models.SET_NULL, null=True)
+    post_date = models.DateField(auto_now_add=True)
+    content = models.TextField(help_text='Type your blog.')
+
+    class Meta:
+        ordering = ['-post_date']
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+    bloguser = models.ForeignKey('BlogUser', on_delete=models.SET_NULL, null=True)
+
+
 class BlogUser(User):
     bio_info = models.TextField(null=True, blank=True, help_text='Enter some biographical information.')
 
