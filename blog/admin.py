b97d5618311e80471d60e10a9034a85a4d4880f3
90houlaoheshang <907333918@qq.com>
2020-06-06 16:08:35 +0800

    添加BlogUser的admin页面

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import BlogUser, Blog
+
+
+class BlogInline(admin.TabularInline):
+    model = Blog
+    extra = 0
+
+
+@admin.register(BlogUser)
+class BlogUserAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'email', 'bio_info']
+    # fields = ['username', ('email', 'bio_info')]
+    fieldsets = (
+        ('称谓', {'fields': ('username',)}),
+        ('详情', {'fields': ('email', 'bio_info')})
+    )
