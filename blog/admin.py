92fcd78381b4e7a321ee4db20b9b51e482d4c2d8
90houlaoheshang <907333918@qq.com>
2020-06-06 16:18:16 +0800

    注册博客、评论到admin页面

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import BlogUser, Blog
+from blog.models import BlogUser, Blog, Comment
+
+admin.site.register(Blog)
+admin.site.register(Comment)
 
 
 class BlogInline(admin.TabularInline):
