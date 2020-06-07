f9a39e28c00ea1be105ec4f1e71fdebe5932473d
90houlaoheshang <907333918@qq.com>
2020-06-07 20:12:13 +0800

    添加BlogListView的测试

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 # Create your tests here.
 from django.urls import reverse
 
-from blog.models import BlogUser
+from blog.models import BlogUser, Blog
 
 
 class IndexViewTest(TestCase):
@@ -79,3 +79,48 @@ class RegisterViewTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(str(response.context['user']), 'testuser')
+
+
+class BlogListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        author = BlogUser(username='testbloguser',
+                          first_name='test',
+                          last_name='bloguser',
+                          password='dflewqlr23ru23u3423ryqe9fyq348g',
+                          email='nkdwkfda@qnewfnaf.com',
+                          bio_info='测试用的博客作者')
+        author.save()
+        for i in range(21):
+            Blog.objects.create(
+                author=author,
+                content=f'这是一个测试用的博客{i}',
+                name=f'测试博客{i}'
+            ).save()
+
+    def test_blog_list_view_is_accessible_at_the_expected_location(self):
+        response = self.client.get('/blog/blogs/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_list_view_accessible_at_the_expected_named_url(self):
+        response = self.client.get(reverse('blogs'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_list_view_uses_the_expected_template(self):
+        response = self.client.get(reverse('blogs'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('blog/blog_list.html')
+
+    def test_blog_list_view_paginates_records_by_5(self):
+        response = self.client.get(reverse('blogs'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'])
+        self.assertEqual(len(response.context['object_list']), 5)
+
+    def test_blog_list_view_lists_all_objects(self):
+        response = self.client.get(reverse('blogs') + '?page=5')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'])
+        self.assertEqual(len(response.context['object_list']), 1)
