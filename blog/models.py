c02e16a208f492b1db0d3a62f36c3cccd7a54244
90houlaoheshang <907333918@qq.com>
2020-06-07 12:45:30 +0800

    添加一些模型字段的长度限制

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Blog(models.Model):
     name = models.CharField(max_length=50, help_text='Enter the name of the blog.')
     author = models.ForeignKey('BlogUser', on_delete=models.SET_NULL, null=True)
     post_date = models.DateField(auto_now_add=True)
-    content = models.TextField(help_text='Type your blog.')
+    content = models.TextField(max_length=5000, help_text='Type your blog.')
 
     class Meta:
         ordering = ['-post_date']
@@ -21,7 +21,7 @@ class Comment(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     post_datetime = models.DateTimeField(auto_now_add=True)
     bloguser = models.ForeignKey('BlogUser', on_delete=models.SET_NULL, null=True)
-    content = models.TextField(help_text='键入对该博客的评论')
+    content = models.TextField(max_length=200, help_text='键入对该博客的评论')
 
     class Meta:
         ordering = ['post_datetime']
@@ -31,7 +31,7 @@ class Comment(models.Model):
 
 
 class BlogUser(User):
-    bio_info = models.TextField(null=True, blank=True, help_text='Enter some biographical information.')
+    bio_info = models.TextField(max_length=200, null=True, blank=True, help_text='Enter some biographical information.')
 
     def __str__(self):
         return f'{self.last_name} {self.first_name}'
