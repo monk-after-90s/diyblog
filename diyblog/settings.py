a3acbcb88b6af9b617aaebbc468f22bebbd8c7ae
90houlaoheshang <907333918@qq.com>
2020-06-15 13:31:58 +0800

    生产环境配置

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&aek2dh%^azgmkcmseqa7+(=&fmy!eaxa^8(&n8gw&sjj$9zg^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
